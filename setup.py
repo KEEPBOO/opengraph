52ed18d5cf49365f77cfed1f46ce8338a6e11099
Oleg Stasula <oleg.stasula@gmail.com>
2014-04-05 09:34:00 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='keepboo-opengraph',
+    version='0.1',
+    packages=['src'],
+    url='https://github.com/KEEPBOO/opengraph',
+    license='',
+    author='Oleg Stasula',
+    author_email='oleg.stasula@gmail.com',
+    description='OpenGraph parser used on KEEPBOO'
+)
